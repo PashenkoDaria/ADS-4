e276cafdd6fed488af342a4bc1f60808bfe7160a
PashenkoDaria <92744987+PashenkoDaria@users.noreply.github.com>
2022-03-27 23:55:35 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,64 +1,46 @@
-// Copyright 2021 NNTU-CS
-int countPairs1(int* arr, int len, int value) {
-int count = 0;
-for (int i = 0; i < len - 1; i++) {
-for (int j = i + 1; j < len; j++) {
-if (arr[i] + arr[j] == value) {
-count += 1;
+int binar(int *arr, int l, int r, int a) {
+    int left = l, right = r, mid = 0; bool f = 0;
+    while (left <= right)  {
+        mid = (left + right) / 2;
+        if (arr[mid] == a) {
+            return binar(arr, l, mid - 1, a) + binar(arr, mid + 1, r, a) + 1;
+        }
+        if (a < arr[mid])
+            right = mid - 1;
+        else
+            left = mid + 1;
+    }
+    return 0;
 }
+
+int countPairs1(int *arr, int len, int value) {
+  int count1 = 0;
+  for (int i = 0; i < len; i++) {
+    for (int k = 0; k < len; k++) {
+      if ((i != k) && (arr[i] + arr[k] == value)) {
+        count1 += 1;
+      }
+    }
+  }
+  return count1 / 2;
 }
+
+int countPairs2(int *arr, int len, int value) {
+  int count2 = 0;
+  for (int i = 0; i < len - 1; i++) {
+    for (int k = len - 1; k > i; k--) {
+      if (arr[i] + arr[k] == value) {
+        count2 += 1;
+      }
+    }
+  }
+  return count2;
 }
-return count;
-}
-int countPairs2(int* arr, int len, int value) {
-int count = 0;
-int temp = 0;
-for (int i = 0; i < len; i++) {
-for (int j = len; j > i; j--) {
-if (arr[i] + arr[j] == value) {
-count += 1;
-}
-}
-}
-return count;
-}
-int countPairs3(int* arr, int len, int value) {
-int count = 0, mid = 0, left = 0, right = 0;
-bool tf = true;
-for (int i = 0; i < len - 1; i++) {
-if (value > arr[i]) {
-left = i + 1;
-right = len;
-while (tf) {
-mid = (left + right) / 2;
-if (value - arr[i] < arr[mid]) {
-right = mid - 1;
-} else if (value - arr[i] > arr[mid]) {
-left = mid + 1;
-} else {
-count += 1;
-for (int k = 1; k < len; k++) {
-if (arr[mid + k] == arr[mid]) {
-count += 1;
-} else {
-break;
-}
-}
-for (int k = 1; k < len - mid; k++) {
-if (arr[mid - k] == arr[mid]) {
-count += 1;
-} else {
-break;
-}
-}
-tf = false;
-}
-if (left > right) {
-tf = false;
-}
-}
-tf = true;
-}
-}
-return count;
+
+int countPairs3(int *arr, int len, int value) {
+  int count3 = 0;
+  for (int i = 0; i < len; i++) {
+      count3 += binar(&arr[i + 1], 0, len - i, value - arr[i]);
+  }
+  return count3;
 }
